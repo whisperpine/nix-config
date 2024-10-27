1912c53fdd59dc8f5538632fc440ee814289b662
Yusong <yusong.lai@icloud.com>
2024-10-27 21:59:34 +0000

    chore: init

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "NixOS configuration of Yusong";
+
+  inputs = {
+    # Use the unstable branch by default.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # The latest stable branch which can be used as a fallback.
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
+    # WSL (windwos subsystem for linxu).
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    # Home Manager used for managing user configuration.
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, nixpkgs-stable, nixos-wsl, home-manager, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      # Pass non-defualt args to modules.
+      specialArgs = {
+        pkgs-stable = import nixpkgs-stable {
+          #inherit system;
+          config.allowUnfree = true;
+        };
+      };
+
+      modules = [
+	./configuration.nix
+        
+	# WSL (Windows Subsystem for Linux).
+        nixos-wsl.nixosModules.default
+        {
+          system.stateVersion = "24.05";
+          wsl.enable = true;
+          wsl.defaultUser = "yusong";
+        }
+
+	# Home Manager as a module.
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.yusong = import ./home.nix;
+	  #home-manager.extraSpecialArgs = inputs;
+        }
+      ];
+    };
+  };
+}
+
---
+++ home.nix
@@ -0,0 +1,82 @@
+{ config, pkgs, pkgs-stable, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.username = "yusong";
+  home.homeDirectory = "/home/yusong";
+  home.enableNixpkgsReleaseCheck = false;
+
+  xresources.properties = {
+    # Set mouse cursor size.
+    "Xcursor.size" = 16;
+    # Set system font size.
+    "Xft.dpi" = 172;
+  };
+
+  home.packages = with pkgs; [
+    neofetch
+    btop
+    kubectl
+    zellij
+    just
+  ];
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    shellAliases = {
+      kc = "kubectl";
+      vim = "nvim";
+    };
+    # .bashrc
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin"
+    '';
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Yusong";
+    userEmail = "yusong.lai@icloud.com";
+    aliases = {
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      pushff = "push --force-with-lease --force-if-includes";
+      sc = "sparse-checkout";
+      sm = "submodule";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      line_break.disabled = true;
+      cmd_duration.disabled = true;
+      battery.disabled = true;
+      character = {
+        success_symbol = "[##](default)";
+        error_symbol = "[##](red)";
+        vimcmd_symbol = "[##](default)";
+      };
+      git_status = {
+        ahead = ">";
+        behind = "<";
+        diverged = "<>";
+        renamed = "r";
+        deleted = "x";
+      };
+    };
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.05";
+}
