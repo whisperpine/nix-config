41038cf2630a5b1b696105c98edeed2fca6ae5ff
Yusong <yusong.lai@icloud.com>
2024-10-30 02:15:32 +0000

    refactor: move the outputs out of flake.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,14 @@
 {
-  description = "NixOS configuration of Yusong";
+  description = "Yusong's NixOS Config";
+
+  outputs = inputs: import ./outputs inputs;
 
   inputs = {
     # Use the unstable branch by default.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     # The latest stable branch which can be used as a fallback.
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
-    # WSL (windwos subsystem for linxu).
+    # WSL (windwos subsystem for Linux).
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     # Home Manager used for managing user configuration.
     home-manager = {
@@ -19,41 +21,5 @@
     };
   };
 
-  outputs = inputs@{
-    self, nixpkgs, nixpkgs-stable, nixos-wsl, home-manager, ...
-  }: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem rec {
-      system = "x86_64-linux";
-
-      # Pass non-defualt args to modules.
-      specialArgs = {
-        pkgs-stable = import nixpkgs-stable {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      };
-
-      modules = [
-        ./system.nix
-
-	      # WSL (Windows Subsystem for Linux).
-        nixos-wsl.nixosModules.default
-        {
-          system.stateVersion = "24.05";
-          wsl.enable = true;
-          wsl.defaultUser = "yusong";
-        }
-
-        # Home Manager as a module.
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.yusong = import ./home;
-	        #home-manager.extraSpecialArgs = inputs;
-        }
-      ];
-    };
-  };
 }
 
--- home/btop.nix
+++ home/btop.nix
@@ -1,7 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-stable, ... }:
 {
   programs.btop = {
     enable = true;
+    # package = pkgs-stable.btop;
     settings = {
       theme_background = false;
       vim_keys = true;
