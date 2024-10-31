b080e937b45474c02ccadd03a99f5715da31ae0e
Yusong <yusong.lai@icloud.com>
2024-10-31 03:22:36 +0000

    fix(wsl): nixos-rebuild failed on WSL

--- outputs/x86_64-linux-wsl.nix
+++ outputs/x86_64-linux-wsl.nix
@@ -9,51 +9,47 @@
       config.allowUnfree = true;
     };
   };
+  configuration = { pkgs, config, ... }: {
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It's perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "24.05";
+    # The platform the configuration will be used on.
+    nixpkgs.hostPlatform = system;
+    # Enable experimental features.
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    # It doesn't need to keep too much generations.
+    boot.loader.systemd-boot.configurationLimit = 10;
+    # Set nushell as the default shell for all users.
+    users.defaultUserShell = pkgs.nushell;
+    # Set system-wide environment variables.
+    environment.variables.EDITOR = "nvim";
+    # Optimise storage.
+    nix.settings.auto-optimise-store = true;
+    # Do garbage collection weekly to keep disk usage low.
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 1w";
+    };
+    # Install packages in operating system.
+    environment.systemPackages = with pkgs; [
+      git
+      wget
+      curl
+      neovim
+      nushell
+    ];
+  };
 in
 nixpkgs.lib.nixosSystem {
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05";
-
-  # Set system architecture.
-  system = system;
-
-  # Enable experimental features.
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Set nushell as the default shell for all users.
-  users.defaultUserShell = pkgs.nushell;
-
-  # It doesn't need to keep too much generations.
-  boot.loader.systemd-boot.configurationLimit = 10;
-
-  # Set system-wide environment variables.
-  environment.variables.EDITOR = "nvim";
-
-  # Install packages in operating system.
-  environment.systemPackages = with pkgs; [
-    git
-    wget
-    curl
-    neovim
-    nushell
-  ];
-
-  # Optimise storage.
-  nix.settings.auto-optimise-store = true;
-
-  # Do garbage collection weekly to keep disk usage low.
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 1w";
-  };
-
   modules = [
+    # This modules id defined above in "let" expression.
+    configuration
+
     # WSL (Windows Subsystem for Linux).
     nixos-wsl.nixosModules.default {
       system.stateVersion = "24.05";
