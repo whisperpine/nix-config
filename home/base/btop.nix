58f79b0cda3067f53e7a364b98e6f4e68c84ce57
Yusong <yusong.lai@icloud.com>
2024-12-03 19:03:43 +0000

    feat(btop): set color theme to everforest-dark-medium

--- home/base/btop.nix
+++ home/base/btop.nix
@@ -4,6 +4,7 @@
     enable = true;
     settings = {
       theme_background = false;
+      color_theme = "everforest-dark-medium";
       vim_keys = true;
       update_ms = 500;
       proc_tree = true;
