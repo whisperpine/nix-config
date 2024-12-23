5bd5696bf473fe6108c07af972ab4b5838fecd6c
Yusong <yusong.lai@icloud.com>
2024-12-23 14:50:00 +0800

    fix(neovim): add im-select.nvim configs on macOS

--- home/nvim/lua/plugins/init.lua
+++ home/nvim/lua/plugins/init.lua
@@ -204,6 +204,9 @@ return {
       if os_info.sysname == "Windows" then
         -- be sure that `im-select.exe` has been added to env var "Path"
         options.default_command = "im-select.exe"
+      elseif os_info.sysname == "Darwin" then
+        -- be sure that `im-select` has been added to env var "PATH"
+        options.default_command = "im-select"
       elseif os_info.sysname == "Linux" and vim.fn.has "wsl" == 1 then
         -- todo: use environment variable to get the path of executable file
         options.default_command = "/mnt/c/Users/yusong/external/bin/im-select.exe"
