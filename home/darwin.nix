494e3647d386ffb19eb49b13b679baef246c82bd
Yusong <yusong.lai@icloud.com>
2025-01-16 07:45:55 +0800

    feat(darwin): add colima, docker, docker-compose
    Use colima to setup container runtime on macOS.

--- home/darwin.nix
+++ home/darwin.nix
@@ -3,7 +3,11 @@ let
   configuration =
     { pkgs, ... }:
     {
-      # home.packages = with pkgs; [ ];
+      home.packages = with pkgs; [
+        colima
+        docker
+        docker-compose
+      ];
     };
 in
 {
