7f86d199014406e000369c85238f9b29f26682b7
Yusong <yusong.lai@icloud.com>
2025-04-10 12:36:14 +0800

    feat(neovim): disable treesitter indent for python

--- home/nvim/lua/configs/nvim-treesitter.lua
+++ home/nvim/lua/configs/nvim-treesitter.lua
@@ -3,59 +3,64 @@ pcall(function()
   dofile(vim.g.base46_cache .. "treesitter")
 end)
 
-return {
-  highlight = {
-    enable = true,
-    use_languagetree = true,
-  },
+local M = {}
 
-  indent = { enable = true },
-
-  -- all the supported languages by tree-sitter:
-  -- https://github.com/nvim-treesitter/nvim-treesitter
-  ensure_installed = {
-    "markdown_inline",
-    "markdown",
-    "javascript",
-    "typescript",
-    "git_config",
-    "git_rebase",
-    "gitattributes",
-    "gitcommit",
-    "gitignore",
-    "editorconfig",
-    "ssh_config",
-    "terraform",
-    "mermaid",
-    "graphql",
-    "svelte",
-    "wgsl_bevy",
-    "wgsl",
-    "c_sharp",
-    "python",
-    "vimdoc",
-    "printf",
-    "latex",
-    "vim",
-    "proto",
-    "nix",
-    "bash",
-    "ini",
-    "regex",
-    "nginx",
-    "just",
-    "sql",
-    "cpp",
-    "rust",
-    "luadoc",
-    "lua",
-    "html",
-    "css",
-    "yaml",
-    "toml",
-    "json",
-    "ruby",
-    "nu",
-    "c",
-  },
+M.highlight = {
+  enable = true,
+  use_languagetree = true,
 }
+
+M.indent = {
+  enable = true, -- this will set `indentexpr`
+  disable = { "python" }, -- disable for python
+}
+
+-- all the supported languages by tree-sitter:
+-- https://github.com/nvim-treesitter/nvim-treesitter
+M.ensure_installed = {
+  "markdown_inline",
+  "markdown",
+  "javascript",
+  "typescript",
+  "git_config",
+  "git_rebase",
+  "gitattributes",
+  "gitcommit",
+  "gitignore",
+  "editorconfig",
+  "ssh_config",
+  "terraform",
+  "mermaid",
+  "graphql",
+  "svelte",
+  "wgsl_bevy",
+  "wgsl",
+  "c_sharp",
+  "python",
+  "vimdoc",
+  "printf",
+  "latex",
+  "vim",
+  "proto",
+  "nix",
+  "bash",
+  "ini",
+  "regex",
+  "nginx",
+  "just",
+  "sql",
+  "cpp",
+  "rust",
+  "luadoc",
+  "lua",
+  "html",
+  "css",
+  "yaml",
+  "toml",
+  "json",
+  "ruby",
+  "nu",
+  "c",
+}
+
+return M
