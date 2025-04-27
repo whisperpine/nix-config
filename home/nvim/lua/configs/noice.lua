fb01218c0e7430c15f12ccfd3909de01937324fe
Yusong <yusong.lai@icloud.com>
2025-04-27 19:27:14 +0800

    feat(neovim): use treesitter to highlight nvim-cmp

--- home/nvim/lua/configs/noice.lua
+++ home/nvim/lua/configs/noice.lua
@@ -5,6 +5,12 @@ M = {
     progress = { enabled = true },
     hover = { enabled = false },
     signature = { enabled = false },
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
   },
 
   -- you can enable a preset for easier configuration
