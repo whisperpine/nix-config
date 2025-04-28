4deeca9dd080fac6be7d6fbc7452b263a2362baa
Yusong <yusong.lai@icloud.com>
2025-04-29 07:05:13 +0800

    feat(pkgs): add zsh in home/base.nix

--- home/base.nix
+++ home/base.nix
@@ -14,6 +14,7 @@
   imports = [
     ./git
     ./gpg
+    ./zsh
     ./bash
     ./btop
     ./yazi
--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -3,12 +3,14 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    autosuggestion.enable = true;
     shellAliases = {
       za = "zellij attach";
       kc = "kubectl";
       lg = "lazygit";
       vim = "nvim";
       ls = "eza";
+      ll = "eza -l --icons";
     };
     # .zprofile
     profileExtra = ''
