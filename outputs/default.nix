ce66bff04a5c12ac27340edf2c75bcefd0c415f6
Yusong <yusong.lai@icloud.com>
2025-05-03 10:25:55 +0800

    feat: setup nix develop environment
    - feat: add commit-msg and pre-commit git hooks

--- outputs/default.nix
+++ outputs/default.nix
@@ -1,5 +1,33 @@
-{ ... }@inputs:
+{ nixpkgs, ... }@inputs:
+let
+  supportedSystems = [
+    "x86_64-linux"
+    "aarch64-linux"
+    "x86_64-darwin"
+    "aarch64-darwin"
+  ];
+  forEachSupportedSystem =
+    f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+in
 {
+  devShells = forEachSupportedSystem (
+    { pkgs }:
+    {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          husky
+          typos
+        ];
+        shellHook = ''
+          # install git hook managed by husky
+          if [ ! -e "./.husky/_" ]; then
+            husky install
+          fi
+        '';
+      };
+    }
+  );
+
   darwinConfigurations = {
     # mac mini
     "creator" = import ./darwin-desktop.nix (
