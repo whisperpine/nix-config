050dc8ceefc90db642c45f6f3732b689fa70788b
Yusong <yusonglai64@gmail.com>
2025-06-06 05:22:14 +0800

    fix(neovim): adapt to the new config structure of avante.nvim
    https://github.com/yetone/avante.nvim/wiki/Provider-configuration-migration-guide

--- home/nvim/lua/plugins/avante.lua
+++ home/nvim/lua/plugins/avante.lua
@@ -28,26 +28,6 @@ end
 plugin.opts = {
   hints = { enabled = false },
   provider = "deepseek",
-  openai = {
-    endpoint = "https://api.openai.com/v1",
-    model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-    timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-    temperature = 0,
-    max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-    reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-  },
-  ollama = {
-    endpoint = "http://127.0.0.1:11434", -- Note that there is no /v1 at the end.
-    model = "qwq:32b",
-  },
-  vendors = {
-    deepseek = {
-      __inherited_from = "openai",
-      api_key_name = "DEEPSEEK_API_KEY",
-      endpoint = "https://api.deepseek.com",
-      model = "deepseek-chat",
-    },
-  },
   windows = {
     width = 50, -- default % based on available width
     sidebar_header = {
@@ -69,6 +49,29 @@ plugin.opts = {
   },
   behaviour = {
     enable_token_counting = false,
+  },
+}
+
+plugin.opts.providers = {
+  openai = {
+    endpoint = "https://api.openai.com/v1",
+    model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+    timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+    extra_request_body = {
+      temperature = 0,
+      max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+      reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    },
+  },
+  ollama = {
+    endpoint = "http://127.0.0.1:11434", -- Note that there is no /v1 at the end.
+    model = "qwq:32b",
+  },
+  deepseek = {
+    __inherited_from = "openai",
+    api_key_name = "DEEPSEEK_API_KEY",
+    endpoint = "https://api.deepseek.com",
+    model = "deepseek-chat",
   },
 }
 
