21bef84ec117b52eed4fea08321eba04efc4a7a0
Yusong <yusonglai64@gmail.com>
2025-06-17 04:52:43 +0800

    feat(neovim): config rust_analyzer, enable "cargo.targetDir"

--- home/nvim/lsp/rust_analyzer.lua
+++ home/nvim/lsp/rust_analyzer.lua
@@ -8,9 +8,15 @@ M = {
 }
 
 M.settings = {
+  -- https://rust-analyzer.github.io/book
   ["rust-analyzer"] = {
     check = { command = "clippy" },
     completion = { callable = { snippets = "none" } },
+    cargo = {
+      -- This prevents rust-analyzer's cargo check and initial build-script and proc-macro building
+      -- from locking the Cargo.lock at the expense of duplicating build artifacts.
+      targetDir = true,
+    },
     -- inlayHints = { bindingModeHints = { enable = true } },
     procMacro = {
       ignored = {
