8f454da126f962a99ea0767acb3226bf10d79997
Yusong <yusonglai64@gmail.com>
2025-08-19 14:23:15 +0800

    feat(zsh): read from ./.zprofile to profileExtra

---
+++ home/zsh/.zprofile
@@ -0,0 +1,2 @@
+export PATH="$PATH:$HOME/bin:$HOME/.local/bin:/opt/homebrew/bin"
+export PATH="$PATH:/etc/nixos/scripts/"
--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -4,6 +4,7 @@
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
+    profileExtra = "${builtins.readFile ./.zprofile}";
     shellAliases = {
       za = "zellij attach";
       kc = "kubectl";
@@ -12,10 +13,5 @@
       e = "eza";
       el = "eza -l";
     };
-    # .zprofile
-    profileExtra = ''
-      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:/opt/homebrew/bin"
-      export PATH="$PATH:/etc/nixos/scripts/"
-    '';
   };
 }
