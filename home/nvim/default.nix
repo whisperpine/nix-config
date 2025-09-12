1cd7b012614dd6caefcffe8b22fd7c83cf0f847a
Yusong <yusonglai64@gmail.com>
2025-09-12 22:15:10 +0800

    feat(neovim): remove dotnet-sdk_9 and csharp-ls packages
    It's preferred to install these packages in the repository level
    (e.g. local dev environment entered by "nix develop"), not globally.

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -80,8 +80,6 @@ in
     rust-analyzer # rust
     terraform-ls # terraform
     basedpyright # python
-    dotnet-sdk_9 # csharp
-    csharp-ls # csharp
     tinymist # typst
     lemminx # xml, svg
     helm-ls # helm
