fb40e5bacb248602ea93a2b27c3913a682063419
Yusong <yusonglai64@gmail.com>
2025-10-09 16:35:39 +0800

    refactor(zellij): use the "config.kdl" file for configurations
    Instead of the previous approach, home-manager.

--- home/zellij/default.nix
+++ home/zellij/default.nix
@@ -1,29 +1,35 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs.zellij = {
-    enable = true;
-    settings = {
-      theme = "custom";
-      scroll_buffer_size = 1500;
-      show_startup_tips = false;
-      ui = {
-        pane_frames = {
-          rounded_corners = false;
-        };
-      };
-      themes.custom = {
-        fg = "#bdbdbd"; # selection foreground
-        bg = "#3f3f3f"; # selection background
-        black = "#161616";
-        red = "#161616";
-        green = "#879c54";
-        yellow = "#e6be6f";
-        blue = "#81a2be";
-        magenta = "#b18fbb";
-        cyan = "#79af98";
-        white = "#bdbdbd";
-        orange = "#de935f";
-      };
-    };
+  # programs.zellij = {
+  #   enable = true;
+  #   settings = {
+  #     theme = "custom";
+  #     scroll_buffer_size = 1500;
+  #     show_startup_tips = false;
+  #     ui = {
+  #       pane_frames = {
+  #         rounded_corners = false;
+  #       };
+  #     };
+  #     themes.custom = {
+  #       fg = "#bdbdbd"; # selection foreground
+  #       bg = "#3f3f3f"; # selection background
+  #       black = "#161616";
+  #       red = "#161616";
+  #       green = "#879c54";
+  #       yellow = "#e6be6f";
+  #       blue = "#81a2be";
+  #       magenta = "#b18fbb";
+  #       cyan = "#79af98";
+  #       white = "#bdbdbd";
+  #       orange = "#de935f";
+  #     };
+  #   };
+  # };
+
+  home.packages = with pkgs; [ zellij ];
+  xdg.configFile.zellij = {
+    source = ./config.kdl;
+    target = "./zellij/config.kdl";
   };
 }
