551b5b6d26d08c3ab2b508677d356f77afbe35c9
Yusong <yusonglai64@gmail.com>
2025-10-09 23:14:55 +0800

    feat(pkgs): create symlink for global bun packages

--- home/bun/default.nix
+++ home/bun/default.nix
@@ -1,4 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+  # NOTE: "/etc/nixos" should be a symbolic link of this repo.
+  bunPath = "/etc/nixos/home/bun/global";
+in
 {
   home.packages = with pkgs; [ bun ];
+
+  # CAUTION: packages declared in ./global/package.json won't be auto installed.
+  # They require manually executing "bun add -g PACKAGE_NAME" or the system services
+  # defined in "THIS_REPO/outputs/modules/services/bun-add.nix" (every time OS starts).
+  # Manually update global packages by "bun update --latest -g".
+  home.file.".bun/install/global" = {
+    source = config.lib.file.mkOutOfStoreSymlink bunPath;
+    recursive = true;
+  };
 }
