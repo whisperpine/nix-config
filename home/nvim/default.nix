d9093a4de3058f845c1cf14d3fd05cb8e08f3915
Yusong <yusonglai64@gmail.com>
2025-10-09 16:06:12 +0800

    chore(pkgs): install ansible-language-server in the stable channel
    "ansible-language-server" has been removed from the unstable channel.

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  pkgs-stable,
   config,
   # neovim-nightly-overlay,
   ...
@@ -28,73 +29,77 @@ in
     ../inotify # file watcher used by LSP
   ];
 
-  home.packages = with pkgs; [
-    # required by neovim plugins
-    lua5_1 # lazy.nvim
-    luajitPackages.luarocks # lazy.nvim
-    tree-sitter # tree-sitter.nvim
-    yarn-berry # markdown-preview.nvim
-    nodejs_22 # markdown-preview.nvim
-    ripgrep # telescope.nvim
-    gnumake # avante.nvim
-    zig # tree-sitter.nvim
-    zk # zk-nvim
+  home.packages =
+    with pkgs;
+    [
+      # required by neovim plugins
+      lua5_1 # lazy.nvim
+      luajitPackages.luarocks # lazy.nvim
+      tree-sitter # tree-sitter.nvim
+      yarn-berry # markdown-preview.nvim
+      nodejs_22 # markdown-preview.nvim
+      ripgrep # telescope.nvim
+      gnumake # avante.nvim
+      zig # tree-sitter.nvim
+      zk # zk-nvim
 
-    # formatters and linters
-    # find all formatters here:
-    # https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters
-    nixfmt-rfc-style # nix formatter
-    markdownlint-cli # markdown
-    ansible-lint # ansible
-    shellcheck # shell linter
-    actionlint # github actions
-    xmlstarlet # xml
-    opentofu # terraform, opentofu
-    rustywind # tailwind
-    typstyle # typst
-    htmlhint # html
-    redocly # openapi
-    rustfmt # rust
-    tflint # terraform
-    sqlfluff # sql
-    # sqruff # sql
-    stylua # lua
-    shfmt # shell
-    typos # spell checker
-    biome # ts, js, css, json, graphql
-    deno # ts, js, json, yaml, css, html
-    ruff # python
-    buf # protobuf
+      # formatters and linters
+      # find all formatters here:
+      # https://github.com/stevearc/conform.nvim?tab=readme-ov-file#formatters
+      nixfmt-rfc-style # nix formatter
+      markdownlint-cli # markdown
+      ansible-lint # ansible
+      shellcheck # shell linter
+      actionlint # github actions
+      xmlstarlet # xml
+      opentofu # terraform, opentofu
+      rustywind # tailwind
+      typstyle # typst
+      htmlhint # html
+      redocly # openapi
+      rustfmt # rust
+      tflint # terraform
+      sqlfluff # sql
+      # sqruff # sql
+      stylua # lua
+      shfmt # shell
+      typos # spell checker
+      biome # ts, js, css, json, graphql
+      deno # ts, js, json, yaml, css, html
+      ruff # python
+      buf # protobuf
 
-    # language servers
-    # find all language servers here:
-    # https://github.com/neovim/nvim-lspconfig
-    dockerfile-language-server # dockerfile
-    vscode-langservers-extracted # json, css, html
-    typescript-language-server # typescript, javascript
-    tailwindcss-language-server # tailwind css
-    ansible-language-server # ansible
-    svelte-language-server # svelte
-    bash-language-server # bash sh shell
-    yaml-language-server # yaml
-    lua-language-server # lua
-    wgsl-analyzer # wgsl
-    rust-analyzer # rust
-    terraform-ls # terraform
-    basedpyright # python
-    tinymist # typst
-    lemminx # xml, svg
-    helm-ls # helm
-    harper # English
-    vale-ls # English
-    vale # English
-    texlab # latex
-    taplo # toml
-    zls # zig
-    nil # nix
+      # language servers
+      # find all language servers here:
+      # https://github.com/neovim/nvim-lspconfig
+      dockerfile-language-server # dockerfile
+      vscode-langservers-extracted # json, css, html
+      typescript-language-server # typescript, javascript
+      tailwindcss-language-server # tailwind css
+      svelte-language-server # svelte
+      bash-language-server # bash sh shell
+      yaml-language-server # yaml
+      lua-language-server # lua
+      wgsl-analyzer # wgsl
+      rust-analyzer # rust
+      terraform-ls # terraform
+      basedpyright # python
+      tinymist # typst
+      lemminx # xml, svg
+      helm-ls # helm
+      harper # English
+      vale-ls # English
+      vale # English
+      texlab # latex
+      taplo # toml
+      zls # zig
+      nil # nix
 
-    # including lsp "clangd" and formatter "clang-format"
-    # lsp: c, cpp, objc. formatter: c, cpp, objc, java, csharp
-    clang-tools
-  ];
+      # including lsp "clangd" and formatter "clang-format"
+      # lsp: c, cpp, objc. formatter: c, cpp, objc, java, csharp
+      clang-tools
+    ]
+    ++ [
+      pkgs-stable.ansible-language-server # ansible
+    ];
 }
