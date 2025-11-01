3ee2766c85c14c47b7bcc0d661a15322147cccea
Yusong <yusonglai64@gmail.com>
2025-11-02 03:49:34 +0800

    feat(git): configure gpg.ssh.allowedSignersFile in .gitconfig
    git config --global gpg.ssh.allowedSignersFile ~/.ssh/allowed_signers

--- home/git/default.nix
+++ home/git/default.nix
@@ -2,6 +2,7 @@
 let
   repoDir = builtins.getEnv "PWD";
   gitconfig = "${repoDir}/home/git/.gitconfig";
+  allowedSigners = "${repoDir}/home/git/allowed_signers";
 in
 {
   home.packages = with pkgs; [
@@ -11,6 +12,7 @@ in
   ];
 
   home.file.".gitconfig".source = config.lib.file.mkOutOfStoreSymlink gitconfig;
+  home.file.".ssh/allowed_signers".source = config.lib.file.mkOutOfStoreSymlink allowedSigners;
 
   xdg.configFile.themes = {
     source = ./themes.gitconfig;
