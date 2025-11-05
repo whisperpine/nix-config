90c02bda8beb712a3ed450e8f68d936d53d414e3
Yusong <yusonglai64@gmail.com>
2025-11-06 06:26:26 +0800

    chore: print the last modified date of "flake.lock"

--- outputs/dev-shells.nix
+++ outputs/dev-shells.nix
@@ -18,14 +18,20 @@ forEachSupportedSystem (
   { pkgs }:
   {
     default = pkgs.mkShell {
+      # The Nix packages installed in the dev environment.
       packages = with pkgs; [
         husky # managing git hooks
         just # just a command runner
         git-cliff # changelog generator
         cocogitto # conventional commit toolkit
       ];
+      # The shell script executed when the environment is activated.
       shellHook = ''
-        # install git hook managed by husky
+        # Print the last modified date of "flake.lock".
+        stat flake.lock | grep "Modify" |
+          awk '{printf "\"flake.lock\" last modified on: %s", $2}' &&
+          echo " ($((($(date +%s) - $(stat -c %Y flake.lock)) / 86400)) days ago)"
+        # Install git hook managed by husky.
         if [ ! -e "./.husky/_" ]; then
           husky install
         fi
