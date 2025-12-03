b9d449f7d178d52a8fb7d13a16fcae300c1fa39e
Yusong <yusonglai64@gmail.com>
2025-12-03 18:48:18 +0800

    feat(nixos): add outputs/modules/services/x11.nix

---
+++ outputs/modules/services/x11.nix
@@ -0,0 +1,16 @@
+{ ... }:
+# ----------  x11 configs ---------- #
+{
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable the GNOME Desktop Environment.
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+}
