0f40d00d3c8e6340a33c65b426b8d401de042364
Yusong <yusonglai64@gmail.com>
2025-12-04 05:38:57 +0800

    feat(ghostty): add the "ghostty" package and basic configs

---
+++ home/ghostty/default.nix
@@ -0,0 +1,12 @@
+{ pkgs, config, ... }:
+let
+  repoDir = builtins.getEnv "PWD";
+  ghosttyConfig = "${repoDir}/home/ghostty/config";
+in
+{
+  home.packages = with pkgs; [ ghostty ];
+  xdg.configFile.ghostty = {
+    source = config.lib.file.mkOutOfStoreSymlink ghosttyConfig;
+    target = "./ghostty/config";
+  };
+}
