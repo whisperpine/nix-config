97205fd9f608f6b1fdb7134aa423abb4c5b62d4a
Yusong <yusonglai64@gmail.com>
2025-12-03 23:44:03 +0800

    feat(desktop): enable wacome tablet support

--- outputs/modules/services/x11.nix
+++ outputs/modules/services/x11.nix
@@ -3,7 +3,12 @@
 {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
+
+  # Use nvidia GPU driver (see also: ../hardware/nvidia.nix).
   services.xserver.videoDrivers = [ "nvidia" ];
+
+  # Enable wacome tablet support.
+  services.xserver.wacom.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
