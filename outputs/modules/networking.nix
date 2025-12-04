b1d9dd43db7ee1e378de6bcaff213624adb53bb6
Yusong <yusonglai64@gmail.com>
2025-12-05 06:29:53 +0800

    feat(nixos): enable network manager

--- outputs/modules/networking.nix
+++ outputs/modules/networking.nix
@@ -1,6 +1,10 @@
 { ... }:
 # ----------  networking configs ---------- #
 {
+  # Enable network manager.
+  networking.networkmanager.enable = true;
+
+  # Setup network proxy (in conjunction with e.g. clash).
   networking.proxy.default = "http://127.0.0.1:7897";
   networking.proxy.noProxy = "127.0.0.1,localhost";
 
