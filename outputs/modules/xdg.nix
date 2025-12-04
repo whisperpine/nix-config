c38d6ca63c8237aafb8904a457396917dc69888c
Yusong <yusonglai64@gmail.com>
2025-12-05 00:01:45 +0800

    feat(nixos): set xdg.portal.wlr.enable to true

--- outputs/modules/xdg.nix
+++ outputs/modules/xdg.nix
@@ -3,12 +3,14 @@
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    # Specify preferred portal implementations:
     # https://mynixos.com/nixpkgs/option/xdg.portal.config
-    config = {
-      # Specify preferred portal implementations.
-      common.default = [ "gtk" ];
-    };
+    config.common.default = [ "gtk" ];
   };
+
+  # Add the xdg-desktop-portal-wlr package into the xdg.portal.extraPortals
+  # option, and provide the configuration file .
+  xdg.portal.wlr.enable = true;
 
   # xdg.portal: since you installed Home Manager via its NixOS module and
   # 'home-manager.useUserPackages' is enabled, you need to add this so that
