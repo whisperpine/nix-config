bf6f8ff76bf765cce16670fac627caeb3feeb1a9
Yusong <yusonglai64@gmail.com>
2025-12-05 23:23:16 +0800

    feat(nixos): update audio configurations

--- outputs/modules/services/peripherals.nix
+++ outputs/modules/services/peripherals.nix
@@ -10,13 +10,27 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  security.rtkit.enable = true;
-  services.pulseaudio.enable = false;
+  # Enable pipewire itself.
   services.pipewire = {
     enable = true;
+    # Enable support for older ALSA clients (low-level access).
     alsa.enable = true;
+    # Recommended for some 32-bit applications (like WINE).
     alsa.support32Bit = true;
+    # Enable support for older PulseAudio clients (most desktop apps).
     pulse.enable = true;
+    # If you need low-latency Pro Audio (like Ardour, Jack).
+    jack.enable = true;
   };
+
+  # Recommended to allow PipeWire to run with real-time scheduling for
+  # lower latency and better audio performance.
+  security.rtkit.enable = true;
+
+  # Disable the older PulseAudio (PipeWire provides pulse support).
+  services.pulseaudio.enable = false;
+
+  # Advanced Linux Sound Architecture (ALSA).
+  # Prevent volume issues on reboot.
+  hardware.alsa.enablePersistence = true;
 }
