d907f5d525d364c05c5ce3931fefb284fc7c16da
Yusong <yusonglai64@gmail.com>
2025-12-06 05:01:15 +0800

    feat(desktop): configure mimeapps.list in home/linux-desktop.nix

--- home/linux-desktop.nix
+++ home/linux-desktop.nix
@@ -7,6 +7,22 @@ in
   home.username = username;
   home.homeDirectory = "/home/${username}";
 
+  xdg.mimeApps.enable = true;
+  # This option creates/manages files in both:
+  # "~/.config/mimeapps.list" and "~/.local/share/applications/mimeapps.list".
+  xdg.mimeApps.defaultApplications = {
+    # zathura
+    "application/pdf" = "zathura.desktop";
+    # clash-verge
+    "x-scheme-handler/clash" = "clash-verge.desktop";
+    "x-scheme-handler/clash-verge" = "clash-verge.desktop";
+    # google-chrome
+    "text/html" = "google-chrome.desktop";
+    "x-scheme-handler/http" = "google-chrome.desktop";
+    "x-scheme-handler/https" = "google-chrome.desktop";
+    "x-scheme-handler/about" = "google-chrome.desktop";
+  };
+
   home.packages =
     with pkgs;
     [
