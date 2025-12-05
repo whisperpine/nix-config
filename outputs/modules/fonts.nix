b0fd98eb8ac5de98cae7c2a8cd4bcb73c04a354d
Yusong <yusonglai64@gmail.com>
2025-12-05 18:58:42 +0800

    feat(nixos): modify font config in output/modules/fonts.nix

--- outputs/modules/fonts.nix
+++ outputs/modules/fonts.nix
@@ -1,10 +1,39 @@
 { pkgs, ... }:
 # ----------  fonts configs ---------- #
 {
+  fonts = {
+    # Keep default fonts installed.
+    enableDefaultPackages = true;
+    # Expose fonts to /run/current-system/sw/share/X11/fonts.
+    fontDir.enable = true;
+  };
+
+  fonts.fontconfig = {
+    enable = true;
+    hinting.enable = false; # for high DPI
+    subpixel.lcdfilter = "none"; # for high DPI
+  };
+
+  fonts.fontconfig.defaultFonts = {
+    sansSerif = [
+      "Noto Sans"
+      "Noto Sans CJK SC"
+      "Noto Sans CJK HK"
+      "Noto Sans CJK TC"
+      "Noto Sans CJK JP"
+      "Noto Sans CJK KR"
+    ];
+    serif = [ "Noto Serif" ];
+    monospace = [ "Noto Sans Mono" ];
+    emoji = [ "Noto Color Emoji" ];
+  };
+
   # Install fonts.
   fonts.packages = with pkgs; [
     nerd-fonts.symbols-only
     sarasa-gothic
     cascadia-code
+    noto-fonts-cjk-sans
+    noto-fonts
   ];
 }
