3d3afcfb36195e6e19f4d81df57b7b08abc0cf2c
Yusong <yusonglai64@gmail.com>
2025-12-05 21:46:55 +0800

    fix(pkgs): discord is not available on "aarch64-linux"

--- home/linux-desktop.nix
+++ home/linux-desktop.nix
@@ -1,14 +1,22 @@
 { username, pkgs, ... }:
+let
+  # pkgs.discord is unfree.
+  discord = if pkgs.stdenv.hostPlatform.system == "aarch64-linux" then pkgs.hello else pkgs.discord;
+in
 {
   home.username = username;
   home.homeDirectory = "/home/${username}";
-  home.packages = with pkgs; [
-    anki
-    ddcutil
-    fuzzel
-    hyprlock
-    discord
-  ];
+
+  home.packages =
+    with pkgs;
+    [
+      anki
+      ddcutil
+      fuzzel
+      hyprlock
+    ]
+    ++ [ discord ];
+
   imports = [
     ./desktop.nix
     ./alacritty
