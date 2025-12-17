bf82c9ce33fdbc601cb67c4ada62448153036e32
Yusong <yusonglai64@gmail.com>
2025-12-18 04:49:27 +0800

    feat(fuzzel): add nerd font icons for all search engines

--- home/fuzzel/fuzzel-dmenu.sh
+++ home/fuzzel/fuzzel-dmenu.sh
@@ -3,21 +3,41 @@
 # Define search engines.
 declare -A engines
 engines=(
-  ["Google"]="https://www.google.com/search?q="
-  ["GitHub"]="https://github.com/search?q="
-  ["YouTube"]="https://www.youtube.com/results?search_query="
-  ["Docs.rs"]="https://docs.rs/releases/search?query="
-  ["Collins Dictionary"]="https://www.collinsdictionary.com/search/?dictCode=english&q="
-  ["PyPI"]="https://pypi.org/search/?q="
-  ["Home Manager"]="https://home-manager-options.extranix.com/?query="
-  ["NixOS Packages"]="https://search.nixos.org/packages?query="
-  ["Google Translate"]="https://translate.google.com/?source=osdd&sl=auto&tl=auto&op=translate&text="
-  # ["GitLab"]="https://gitlab.com/search?search="
-  # ["lib.rs"]="https://lib.rs/search?q="
-  # ["crates.io"]="https://crates.io/search?q="
-  # ["tldr"]="https://tldr.inbrowser.app/search?query="
-  # ["Can I use"]="https://caniuse.com/?search="
-  # ["X"]="https://x.com/search?q="
+  # --- search engine --- #
+  ["  Google"]="https://www.google.com/search?q="
+  ["󰇥  DuckDuckGo"]="https://duckduckgo.com/?&q="
+  # ["󰂤  Bing"]="https://www.bing.com/search?q="
+
+  # --- social media --- #
+  ["󰗃  YouTube"]="https://www.youtube.com/results?search_query="
+  ["  Steam"]="https://store.steampowered.com/search?term="
+  ["  Reddit"]="https://www.reddit.com/search/?q="
+  ["  Twitter X"]="https://x.com/search?q="
+
+  # --- dictionary --- #
+  ["󰺄  Collins Dictionary"]="https://www.collinsdictionary.com/search/?dictCode=english&q="
+  ["󰊿  Google Translate"]="https://translate.google.com/?source=osdd&sl=auto&tl=auto&op=translate&text="
+
+  # --- documents --- #
+  ["  Nerd Fonts"]="https://www.nerdfonts.com/cheat-sheet?q="
+  ["󱄅  Home Manager"]="https://home-manager-options.extranix.com/?query="
+  ["󱄅  NixOS Packages"]="https://search.nixos.org/packages?query="
+  ["󱉟  MDN"]="https://developer.mozilla.org/en-US/search?q="
+  ["  Docs.rs"]="https://docs.rs/releases/search?query="
+  # ["  tldr"]="https://tldr.inbrowser.app/search?query="
+  # ["  Can I use"]="https://caniuse.com/?search="
+
+  # --- package registry --- #
+  ["  PyPI"]="https://pypi.org/search/?q="
+  ["󰡨  Docker Hub"]="https://hub.docker.com/search?q="
+  ["  lib.rs"]="https://lib.rs/search?q="
+  ["  crates.io"]="https://crates.io/search?q="
+  ["  npm"]="https://www.npmjs.com/search?q="
+
+  # --- code bases --- #
+  ["  GitHub"]="https://github.com/search?q="
+  # ["  GitLab"]="https://gitlab.com/search?search="
+  # ["  Codeberg"]="https://codeberg.org/explore/repos?only_show_relevant=true&sort=moststars&q="
 )
 
 # Choose the search engine.
