d805a333b3049b720aba7037d94398fba0c9e06c
Yusong <yusonglai64@gmail.com>
2025-12-18 06:02:01 +0800

    feat(desktop): use downsized icon for OpenTabletDriver

--- home/otd/default.nix
+++ home/otd/default.nix
@@ -1,5 +1,5 @@
 { ... }:
 {
   # This will install the icon to "~/.local/share/icons/otd.png".
-  xdg.dataFile."icons/otd.png".source = ./otd.png;
+  xdg.dataFile."icons/otd.png".source = ./otd-64x64.png;
 }
