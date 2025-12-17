6bdd3fea3fd50be310aa9298bd99160f90501915
Yusong <yusonglai64@gmail.com>
2025-12-18 01:36:58 +0800

    feat(git): use "mergiraf" as the default merge conflict driver

--- home/git/default.nix
+++ home/git/default.nix
@@ -4,19 +4,32 @@ let
   repoDir = builtins.getEnv "PWD";
   gitconfig = "${repoDir}/home/git/.gitconfig";
   allowedSigners = "${repoDir}/home/git/allowed_signers";
+  gitThemesConfig = "${repoDir}/home/git/themes.gitconfig";
+  gitattributes = "${repoDir}/home/git/.gitattributes";
 in
 {
   home.packages = with pkgs; [
     git
-    delta
-    git-lfs
+    git-lfs # large file storage
+    delta # syntax-highlighting pager
+    mergiraf # syntax-aware git merge driver
   ];
 
+  # "~/.gitconfig"
   home.file.".gitconfig".source = config.lib.file.mkOutOfStoreSymlink gitconfig;
+
+  # "~/.ssh/allowed_signers"
   home.file.".ssh/allowed_signers".source = config.lib.file.mkOutOfStoreSymlink allowedSigners;
 
-  xdg.configFile.themes = {
-    source = ./themes.gitconfig;
+  # "~/.config/git/attributes"
+  xdg.configFile.gitattributes = {
+    source = config.lib.file.mkOutOfStoreSymlink gitattributes;
+    target = "./git/attributes";
+  };
+
+  # "~/.config/git/themes.gitconfig"
+  xdg.configFile.gitThemes = {
+    source = config.lib.file.mkOutOfStoreSymlink gitThemesConfig;
     target = "./git/themes.gitconfig";
   };
 }
