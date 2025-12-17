02f6698aa4c7a244cbaaa4339c3290e1c0ff4347
Yusong <yusonglai64@gmail.com>
2025-12-18 07:53:43 +0800

    feat(desktop): set gtk's icon theme as "Adwaita"

--- home/gtk/default.nix
+++ home/gtk/default.nix
@@ -1,9 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # GTK requires dconf.
   gtk = {
     enable = true;
     # System settings for dark theme.
     colorScheme = "dark";
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.adwaita-icon-theme;
+    };
   };
 }
