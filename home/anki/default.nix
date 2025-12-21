e8f99b2b7188e4d9f285628ae3772452c8e8114e
Yusong <yusonglai64@gmail.com>
2025-12-22 07:14:37 +0800

    feat(pkgs): force anki to use wayland "ANKI_WAYLAND=1"

--- home/anki/default.nix
+++ home/anki/default.nix
@@ -2,6 +2,10 @@
 {
   home.packages = with pkgs; [ anki ];
 
+  # Force Anki to use Wayland.
+  # https://docs.ankiweb.net/platform/linux/wayland.html
+  home.sessionVariables.ANKI_WAYLAND = "1";
+
   # This will install the icon to "~/.local/share/icons/anki.png".
   xdg.dataFile."icons/anki.png".source = ./anki.png;
 }
