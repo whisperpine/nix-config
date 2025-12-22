8c8f665014d1fb7b0a3a0d2ae3ea2930609520b5
Yusong <yusonglai64@gmail.com>
2025-12-22 09:40:57 +0800

    feat(desktop): use nvidia's open kernel modules

--- outputs/modules/hardware/nvidia.nix
+++ outputs/modules/hardware/nvidia.nix
@@ -8,6 +8,6 @@
     modesetting.enable = true;
     # Making sure to use the proprietary drivers until the following issue is fixed:
     # https://github.com/NVIDIA/open-gpu-kernel-modules/issues/472
-    open = false;
+    open = true;
   };
 }
