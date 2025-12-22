2c1805d5be3a8037964a041aaecdf2b90df0b02a
Yusong <yusonglai64@gmail.com>
2025-12-22 14:49:59 +0800

    fix(desktop): only install steam when the platform is x86_64-linux

--- outputs/modules/steam.nix
+++ outputs/modules/steam.nix
@@ -1,10 +1,15 @@
-{ ... }:
+{ pkgs, lib, ... }:
 # ----------  steam configs ---------- #
+let
+  # Check if the system is x86_64-linux.
+  isX86_64 = pkgs.stdenv.hostPlatform.system == "x86_64-linux";
+in
 {
-  # Enable steam.
-  programs.steam.enable = true;
-
-  # Enable Gamescope (highly recommended for Wayland/Niri).
-  # This allows you to run games in a micro-compositor to fix resolution/focus issues.
-  programs.steam.gamescopeSession.enable = true;
+  config = lib.mkIf isX86_64 {
+    # Enable steam.
+    programs.steam.enable = true;
+    # Enable Gamescope (highly recommended for Wayland/Niri).
+    # This allows you to run games in a micro-compositor to fix resolution/focus issues.
+    programs.steam.gamescopeSession.enable = true;
+  };
 }
