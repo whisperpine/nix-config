e32c6536e07738fffc03dd1ad4c0f9682c0d4e77
Yusong <yusonglai64@gmail.com>
2025-12-24 06:10:29 +0800

    feat(onedrive): add a systemd service to run "onedrive --monitor"

--- home/onedrive/default.nix
+++ home/onedrive/default.nix
@@ -1,5 +1,7 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 # --- onedrive client for linux --- #
+# To learn how to use, refer to the official docs:
+# https://github.com/abraunegg/onedrive/blob/master/docs/usage.md
 let
   repoDir = builtins.getEnv "PWD";
   onedriveConfig = "${repoDir}/home/onedrive/config";
@@ -16,5 +18,23 @@ in
   xdg.configFile.onedriveSyncList = {
     source = config.lib.file.mkOutOfStoreSymlink syncListConfig;
     target = "./onedrive/sync_list";
+  };
+
+  systemd.user.services.onedrive = {
+    Unit = {
+      Description = "OneDrive Linux client";
+      After = [ "network-online.target" ];
+      Wants = [ "network-online.target" ];
+    };
+
+    Service = {
+      ExecStart = "${pkgs.onedrive}/bin/onedrive --monitor";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
   };
 }
