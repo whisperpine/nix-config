a46eebd11f249cd28fa483688f33cb63c4596fc1
Yusong <yusonglai64@gmail.com>
2025-12-27 20:17:16 +0800

    chore(neovim): add more semantic highlighting rules for rust

--- home/nvim/lua/highlight.lua
+++ home/nvim/lua/highlight.lua
@@ -3,41 +3,48 @@
 
 local hl = vim.api.nvim_set_hl
 
--- Common --
+----- Common -----
 hl(0, "DiagnosticUnnecessary", { link = "" })
 hl(0, "DiagnosticUnderlineHint", { underdotted = true })
 hl(0, "DiagnosticUnderlineWarn", { underdotted = true })
 hl(0, "DiagnosticUnnecessary", { underdotted = true })
 hl(0, "DiagnosticUnderlineError", { bg = "#cc6666", foreground = "#202020" })
 
--- Lua --
+----- Lua -----
 hl(0, "@lsp.type.variable.lua", { link = "" })
 hl(0, "@lsp.mod.global.lua", { link = "@module" })
 hl(0, "@lsp.type.function.lua", { link = "@function" })
 hl(0, "@lsp.typemod.function.defaultLibrary.lua", { link = "@function.builtin" })
 
--- Python --
+----- Python -----
 hl(0, "@lsp.type.variable.python", { link = "" })
 hl(0, "@lsp.type.class.python", { link = "" })
 
--- TypeScript ---
-
+----- TypeScript ------
 hl(0, "@lsp.type.class.typescript", { link = "" })
 hl(0, "@lsp.type.variable.typescript", { link = "" })
 hl(0, "@lsp.typemod.property.static.typescript", { link = "@constant" })
 
--- Rust --
+----- Rust -----
 hl(0, "@lsp.type.keyword.rust", { link = "" })
 hl(0, "@lsp.type.decorator.rust", { link = "" })
 hl(0, "@lsp.type.punctuation.rust", { link = "@punctuation" })
 hl(0, "@lsp.type.builtinAttribute.rust", { link = "@function.builtin" }) -- e.g. #[cfg_attr(..)]
-hl(0, "@lsp.typemod.keyword.injected.rust", { link = "@keyword" }) -- keyword in comments
-hl(0, "@lsp.typemod.const.injected.rust", { link = "@variable" }) -- code blocks in comments
 hl(0, "@lsp.typemod.parameter.mutable.rust", { underline = true }) -- e.g. (mut var, ...)
 hl(0, "@lsp.typemod.method.mutable.rust", { underline = true }) -- e.g. fn (&mut self, ...)
 hl(0, "@lsp.typemod.variable.mutable.rust", { underline = true }) -- mut var, &mut var
 hl(0, "@lsp.typemod.operator.mutable.rust", { underline = true }) -- e.g. +=, -=
 hl(0, "@lsp.mod.controlFlow.rust", { link = "@keyword" }) -- e.g. for, in, ?
-hl(0, "@lsp.type.interface.rust", { fg = "#f08368" }) -- trait
+hl(0, "@lsp.type.interface.rust", { fg = "#f0795b" }) -- trait
 hl(0, "@lsp.type.derive.rust", { link = "@lsp.type.interface.rust" })
 hl(0, "@lsp.typemod.typeAlias.associated.rust", { link = "@variable.builtin" })
+hl(0, "@lsp.type.lifetime.rust", { link = "Special" })
+hl(0, "@lsp.type.selfKeyword.rust", { link = "@variable.builtin" }) -- self
+hl(0, "@lsp.type.selfTypeKeyword.rust", { link = "@type" }) -- Self
+-- Rust injected semantic tokens (in code blocks of docstring).
+hl(0, "@lsp.typemod.keyword.injected.rust", { link = "@keyword" }) -- keyword in comments
+hl(0, "@lsp.typemod.const.injected.rust", { link = "@variable" }) -- code blocks in comments
+hl(0, "@lsp.typemod.generic.injected.rust", { link = "@variable" })
+hl(0, "@lsp.typemod.attributeBracket.injected.rust", { link = "@punctuation" })
+hl(0, "@lsp.typemod.typeAlias.injected.rust", { link = "@type" })
+hl(0, "@lsp.typemod.builtinType.injected.rust", { link = "@type" })
