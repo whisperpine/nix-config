50841725456b779481b323ae784a47a4ffe9eeb8
Yusong <yusonglai64@gmail.com>
2025-12-30 19:43:49 +0800

    feat(desktop): use customized udiskie systemd service instead

--- home/udiskie/default.nix
+++ home/udiskie/default.nix
@@ -1,8 +1,30 @@
-{ ... }:
+{ pkgs, ... }:
 # --- disk automounter for udisks --- #
 {
-  services.udiskie = {
-    enable = true;
-    notify = false;
+  home.packages = with pkgs; [ udiskie ];
+
+  # The systemd service to auto start udiskie.
+  # Important: udiskie should be started AFTER the wayland composer,
+  # so that the tray can be shown properly.
+  systemd.user.services.udiskie = {
+    Unit = {
+      Description = "udiskie mount daemon";
+      After = [
+        "tray.target"
+        "xdg-desktop-autostart.target"
+      ];
+      Requires = [
+        "tray.target"
+        "xdg-desktop-autostart.target"
+      ];
+    };
+    Service = {
+      ExecStart = "${pkgs.udiskie}/bin/udiskie --no-notify --smart-tray";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+    Install = {
+      WantedBy = [ "xdg-desktop-autostart.target" ];
+    };
   };
 }
