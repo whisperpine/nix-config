3fee01b91768de7956a6dc1efc0be34792a2f875
Yusong <yusonglai64@gmail.com>
2026-01-12 19:29:56 +0800

    chore(alacritty): use a symbolic link for the alacritty config file

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -1,5 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 # --- terminal emulator --- #
+let
+  repoDir = builtins.getEnv "PWD";
+  alacrittyConfig = "${repoDir}/home/alacritty/alacritty.toml";
+in
 {
   # If alacritty is installed in system level,
   # there's no need to install it by home-manager.
@@ -7,7 +11,7 @@
   home.packages = with pkgs; [ alacritty ];
 
   xdg.configFile.alacritty = {
-    source = ./alacritty.toml;
+    source = config.lib.file.mkOutOfStoreSymlink alacrittyConfig;
     target = "./alacritty/alacritty.toml";
   };
 }
