21466966c9e6d55f58120c1d38d80fe2809c905c
Yusong <yusonglai64@gmail.com>
2026-01-19 18:32:27 +0800

    feat(nixos): configure the trusted network interface

--- outputs/modules/networking.nix
+++ outputs/modules/networking.nix
@@ -13,10 +13,20 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost";
 
   # Enable clash-verge-rev.
-  programs.clash-verge.enable = true;
+  programs.clash-verge = {
+    enable = true;
+    tunMode = true;
+    serviceMode = true;
+  };
   # This option modifies "/etc/xdg/mimeapps.list
   xdg.mime.defaultApplications = {
     "x-scheme-handler/clash" = "clash-verge.desktop";
     "x-scheme-handler/clash-verge" = "clash-verge.desktop";
+  };
+
+  networking.firewall = {
+    # Trust the TUN interface so reverse path filter & input don't drop packets.
+    # IMPORTANT: it's required to configure this to use the TUN mode of clash-verge.
+    trustedInterfaces = [ "Meta" ]; # "Mihomo" or "Meta" — check `ip link` after start
   };
 }
