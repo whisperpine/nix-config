221ebd065c2c7f2b764a9f077d5645ba34bfbf00
Yusong <yusonglai64@gmail.com>
2026-02-01 07:02:12 +0800

    feat(neovim): override treesitter highlights for ecma

---
+++ home/nvim/queries/ecma/highlights.scm
@@ -0,0 +1,16 @@
+; extends
+
+([
+  (this)
+  (super)
+] @variable.builtin
+  (#set! priority 150))
+
+; (method_definition
+;   name: (property_identifier) @constructor
+;   (#eq? @constructor "constructor")
+;   (#set! priority 150))
+;
+; (new_expression
+;   constructor: (identifier) @constructor
+;   (#set! priority 150))
