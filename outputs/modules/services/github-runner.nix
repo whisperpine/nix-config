84d719747987547992099299e3c22e7104661f4f
Yusong <yusonglai64@gmail.com>
2026-03-24 19:11:06 +0800

    feat(github-runner): add multiple ephemeral runners

--- outputs/modules/services/github-runner.nix
+++ outputs/modules/services/github-runner.nix
@@ -1,37 +1,61 @@
-{ config, hostname, ... }:
+{
+  lib,
+  config,
+  hostname,
+  ...
+}:
 # ---------- github-runner configs ---------- #
 # Refer to the following NixOS options:
 # https://search.nixos.org/options?channel=unstable&query=github-runner
+let
+  everbidOrg = index: {
+    services.github-runners."everbid-${index}" = {
+      enable = true;
+      replace = true;
+      ephemeral = true;
+      name = "nixos-${hostname}-${index}";
+      tokenFile = config.sops.secrets."github-runner-token/everbid/org".path;
+      url = "https://github.com/everbid";
+    };
+  };
+  whisperpineNotes = index: {
+    services.github-runners."whisperpine-notes-${index}" = {
+      enable = true;
+      replace = true;
+      ephemeral = true;
+      name = "nixos-${hostname}-${index}";
+      tokenFile = config.sops.secrets."github-runner-token/whisperpine/notes".path;
+      url = "https://github.com/whisperpine/notes";
+    };
+  };
+  whisperpineResume = index: {
+    services.github-runners."whisperpine-resume-${index}" = {
+      enable = true;
+      replace = true;
+      ephemeral = true;
+      name = "nixos-${hostname}-${index}";
+      tokenFile = config.sops.secrets."github-runner-token/whisperpine/resume".path;
+      url = "https://github.com/whisperpine/resume";
+    };
+  };
+  whisperpineBusinessEmail = index: {
+    services.github-runners."whisperpine-business-email-${index}" = {
+      enable = true;
+      replace = true;
+      ephemeral = true;
+      name = "nixos-${hostname}-${index}";
+      tokenFile = config.sops.secrets."github-runner-token/whisperpine/business-email".path;
+      url = "https://github.com/whisperpine/business-email";
+    };
+  };
+in
 {
-  services.github-runners."everbid" = {
-    enable = true;
-    name = "nixos-${hostname}";
-    tokenFile = config.sops.secrets."github-runner-token/everbid/org".path;
-    url = "https://github.com/everbid";
-    replace = true;
-  };
-
-  services.github-runners."whisperpine-notes" = {
-    enable = true;
-    name = "nixos-${hostname}";
-    tokenFile = config.sops.secrets."github-runner-token/whisperpine/notes".path;
-    url = "https://github.com/whisperpine/notes";
-    replace = true;
-  };
-
-  services.github-runners."whisperpine-resume" = {
-    enable = true;
-    name = "nixos-${hostname}";
-    tokenFile = config.sops.secrets."github-runner-token/whisperpine/resume".path;
-    url = "https://github.com/whisperpine/resume";
-    replace = true;
-  };
-
-  services.github-runners."whisperpine-business-email" = {
-    enable = true;
-    name = "nixos-${hostname}";
-    tokenFile = config.sops.secrets."github-runner-token/whisperpine/business-email".path;
-    url = "https://github.com/whisperpine/business-email";
-    replace = true;
-  };
+  config = lib.mkMerge [
+    (everbidOrg "0")
+    (everbidOrg "1")
+    (everbidOrg "2")
+    (whisperpineNotes "0")
+    (whisperpineResume "0")
+    (whisperpineBusinessEmail "0")
+  ];
 }
