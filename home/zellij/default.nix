c084d6b6612334b7018673305dc061f9a1f02431
Yusong <yusonglai64@gmail.com>
2026-04-29 03:29:57 +0800

    chore(zellij): degrade to the stable channel of pkgs

--- home/zellij/default.nix
+++ home/zellij/default.nix
@@ -1,11 +1,11 @@
-{ pkgs, config, ... }:
+{ pkgs-stable, config, ... }:
 # --- terminal multiplexer --- #
 let
   repoDir = builtins.getEnv "PWD";
   zellijConfig = "${repoDir}/home/zellij/config.kdl";
 in
 {
-  home.packages = with pkgs; [ zellij ];
+  home.packages = with pkgs-stable; [ zellij ];
   xdg.configFile.zellij = {
     source = config.lib.file.mkOutOfStoreSymlink zellijConfig;
     target = "./zellij/config.kdl";
