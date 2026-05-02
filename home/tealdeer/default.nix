7ac98a3dd45a85a0b70638f1a5dfa7f90989ea70
Yusong <yusonglai64@gmail.com>
2026-05-03 04:40:35 +0800

    chore(pkgs): modify configs for tealdeer

--- home/tealdeer/default.nix
+++ home/tealdeer/default.nix
@@ -1,10 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 # --- fast tldr implementation --- #
+let
+  repoDir = builtins.getEnv "PWD";
+  tealdeerConfig = "${repoDir}/home/tealdeer/config.toml";
+in
 {
   home.packages = [ pkgs.tealdeer ];
 
   xdg.configFile.tealdeer = {
-    source = ./config.toml;
+    source = config.lib.file.mkOutOfStoreSymlink tealdeerConfig;
     target = "./tealdeer/config.toml";
   };
 }
