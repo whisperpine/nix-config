f3b7fb3dbf1be1cd662983c120d37602350fa508
Yusong <yusonglai64@gmail.com>
2026-05-02 14:25:34 +0800

    chore(desktop): use the latest production version of nvidia driver
    Previously, "legacy_580" was installed to to work around the issue which
    made the kernel crash when Anki was launched.

--- outputs/modules/hardware/nvidia.nix
+++ outputs/modules/hardware/nvidia.nix
@@ -1,4 +1,7 @@
-{ config, ... }:
+{
+  # config,
+  ...
+}:
 # ----------  nvidia configs ---------- #
 {
   # Enables builds with CUDA support with a nixpkgs wide configuration.
@@ -13,7 +16,7 @@
     # Making sure to use the proprietary drivers until the following issue is fixed:
     # https://github.com/NVIDIA/open-gpu-kernel-modules/issues/472
     open = true;
-    # Version 595 has compatibility issues (kernel panics when running Anki).
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+    # # Specify a the driver version.
+    # package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
   };
 }
