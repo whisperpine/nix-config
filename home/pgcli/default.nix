ed25fd741eff9397631629b7784cbae9394125d8
Yusong <yusonglai64@gmail.com>
2026-05-17 13:13:04 +0800

    chore(pgcli): create symlink to "~/.config/pgcli/config"

--- home/pgcli/default.nix
+++ home/pgcli/default.nix
@@ -1,12 +1,16 @@
-{ ... }:
+{ config, ... }:
 # --- cli for postgresql --- #
+let
+  repoDir = builtins.getEnv "PWD";
+  pgcliConfig = "${repoDir}/home/pgcli/config";
+in
 {
   # # NOTE: pgcli should be installed in repo level,
   # # thereby the following line is commented.
   # home.packages = with pkgs; [ pgcli ];
 
   xdg.configFile.pgcli = {
-    source = ./config;
+    source = config.lib.file.mkOutOfStoreSymlink pgcliConfig;
     target = "./pgcli/config";
   };
 }
