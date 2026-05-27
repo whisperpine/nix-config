99faff2e712500a20f85123f4140dfd3258f0f56
Yusong <yusonglai64@gmail.com>
2026-05-27 09:07:41 +0800

    chore(zellij): install zellij from the unstable channel of nixpkgs

--- home/zellij/default.nix
+++ home/zellij/default.nix
@@ -1,11 +1,11 @@
-{ pkgs-stable, config, ... }:
+{ pkgs, config, ... }:
 # --- terminal multiplexer --- #
 let
   repoDir = builtins.getEnv "PWD";
   zellijConfig = "${repoDir}/home/zellij/config.kdl";
 in
 {
-  home.packages = with pkgs-stable; [ zellij ];
+  home.packages = with pkgs; [ zellij ];
   xdg.configFile.zellij = {
     source = config.lib.file.mkOutOfStoreSymlink zellijConfig;
     target = "./zellij/config.kdl";
