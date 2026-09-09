67ded15615ac2d4a285574379eea5bbc3b6508d4
Yusong <yusonglai64@gmail.com>
2026-09-09 12:24:34 +0300

    chore(nixos): adapt to breaking changes of "services.journald"

--- outputs/modules/services/journald.nix
+++ outputs/modules/services/journald.nix
@@ -5,21 +5,16 @@
   # https://manpages.debian.org/testing/systemd/journald.conf.5.en.html
 
   # This config modifies "/etc/systemd/journald.conf".
-  services.journald.extraConfig = ''
-
-    # --- services.journald.extraConfig --- #
-
+  services.journald.settings.Journal = {
     # Limits the total log size.
-    SystemMaxUse=1G
+    SystemMaxUse = "1G";
 
     # How large an individual log file can get before
     # it's "rotated" and becomes eligible for cleanup.
-    SystemMaxFileSize=50M
+    SystemMaxFileSize = "50M";
 
     # Automatically deletes logs older than 30 days,
     # even if you haven't hit the "SystemMaxUse" limit.
-    MaxRetentionSec=1month
-
-    # --- services.journald.extraConfig --- #
-  '';
+    MaxRetentionSec = "1month";
+  };
 }
