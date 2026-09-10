db18bbfb650f7caebef27408f4e7e531dceb4bcb
Yusong <yusonglai64@gmail.com>
2026-09-10 23:57:31 +0300

    feat(nixos): use the master branch of home-manager

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
     };
     # Home Manager used for managing user configuration.
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       # The `follows` keyword in inputs is used for inheritance.
       # `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
